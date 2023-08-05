e4f20e7f6bcb10e05d952571bf138deb61bb70c1
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2023-08-05 21:40:45 +0300

    Installed material ui icons

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <h3>Where in the world?</h3>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default navbar
