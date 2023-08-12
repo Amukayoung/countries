448b7601578c6730e3b4929486595773f75c1f30
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2023-08-12 23:49:24 +0300

    setup routes with react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes,Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import "./App.css"
+
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+      
     </div>
   );
 }
