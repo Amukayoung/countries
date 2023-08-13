77823f037c8c735da23a97c3e23bbeef608d091b
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2023-08-13 12:22:00 +0300

    Added a back button

--- src/Pages/CountryDetails.js
+++ src/Pages/CountryDetails.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TopNavbar from '../components/TopNavbar'
+import LinkButton from '../components/LinkButton'
 
 const CountryDetails = () => {
   return (
     <div>
         <TopNavbar/>
+        <LinkButton/>
     </div>
   )
 }
