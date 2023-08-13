d68d6f4dbc90695305f58ff3ef7785adbb6c4d0c
Andrew Amuka | Software Dev | JavaScript & Java <105834291+Amukayoung@users.noreply.github.com>
2023-08-13 11:09:56 +0300

    Added country details route
    Added the countrydetails component and dreated a route to it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes,Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import "./App.css"
+import CountryDetails from "./Pages/CountryDetails";
 
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/details" element={<CountryDetails/>}/>
+
       </Routes>
       
     </div>
---
+++ src/Pages/CountryDetails.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopNavbar from '../components/TopNavbar'
+
+const CountryDetails = () => {
+  return (
+    <div>
+        <TopNavbar/>
+    </div>
+  )
+}
+
+export default CountryDetails
